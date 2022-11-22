01b6247403f814395fd42b0e6397f29b9024c617
Wen-Heng (Jack) Chung <whchung@gmail.com>
2022-11-22 13:06:44 +0000

    Reduce build time even further

--- src/enqueue.cc
+++ src/enqueue.cc
@@ -77,9 +77,9 @@ typedef void(*ncclKern_t)(struct ncclDevComm* comm, uint64_t channelMask, struct
 // Must be consistent with the ncclFuncSet enum
 static ncclKern_t const ncclKerns[4] = {
   NCCL_KERN_NAME(SendRecv, RING, SIMPLE, Sum, int8_t),
-  NCCL_KERN_NAME_DEBUG(SendRecv, RING, SIMPLE, Sum, int8_t),
-  NCCL_KERN_NAME_LL128(SendRecv, RING, SIMPLE, Sum, int8_t),
-  NCCL_KERN_NAME_LL128_DEBUG(SendRecv, RING, SIMPLE, Sum, int8_t),
+  //NCCL_KERN_NAME_DEBUG(SendRecv, RING, SIMPLE, Sum, int8_t),
+  //NCCL_KERN_NAME_LL128(SendRecv, RING, SIMPLE, Sum, int8_t),
+  //NCCL_KERN_NAME_LL128_DEBUG(SendRecv, RING, SIMPLE, Sum, int8_t),
 };
 
 static ncclResult_t computeColl(struct ncclInfo* info /* input */, int* workFuncIndex, struct ncclWorkElem* work, struct ncclProxyOp* proxyOp /* output */);
